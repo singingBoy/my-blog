a55ba7b8b8200a452f8bcb3f6e3a664012043346
wangdesheng <737708900@qq.com>
2018-05-01 18:06:53 +0800

    add SvgIcon

---
+++ src/components/common/SvgIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="default" :style="iconStyle" :fill="iconColor" aria-hidden="true">
+    <use :xlink:href="`#${iconName}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    },
+    iconStyle: String,
+    iconColor: {
+      type: String,
+      default: '#707070'
+    }
+  }
+}
+</script>
+<style scoped>
+  .default{
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+  }
+</style>
